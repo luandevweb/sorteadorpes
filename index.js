1b55b6ea4f83049fa53afce854073e27ee9f5a9d
Luan DevWeb <luandevweb@gmail.com>
2025-06-26 21:01:39 -0300

    atualizacoes

--- index.js
+++ index.js
@@ -1,44 +1,104 @@
 
-var verificaContinente = localStorage.getItem("continente")
+
+var verificaContinente = sessionStorage.getItem("continente")
 
 if(verificaContinente){
-    document.getElementById("um").style.display = "none"
+
+    mostrarCarregando()
+
+    if(verificaContinente === "Europa"){
+
+         var capturaAudio = "audio/champions.mp3"
+            var player = document.getElementById("audioteste")
+
+            player.src = capturaAudio
+            player.play()
+
+    }
+
+     if(verificaContinente === "America Do Sul"){
+
+         var capturaAudio = "audio/libertadores.mp3"
+            var player = document.getElementById("audioteste")
+
+            player.src = capturaAudio
+            player.play()
+
+    }
+
+
+    if(verificaContinente === "FifaCup"){
+
+         var capturaAudio = "audio/mundial.mp3"
+            var player = document.getElementById("audioteste")
+
+            player.src = capturaAudio
+            player.play()
+
+    }
+
+    if(verificaContinente === "Brasileirao"){
+
+         var capturaAudio = "audio/brasileirao.mpeg"
+            var player = document.getElementById("audioteste")
+
+            player.src = capturaAudio
+            player.play()
+
+    }
+
+
+    setTimeout(() => {
+         document.getElementById("carregando-campeonato").style.display = "none"
+         document.getElementById("um").style.display = "none"
     document.getElementById("dois").style.display = "block"
+
+    var capturaAudio = "audio/sortear.m4a"
+            var player = document.getElementById("audioteste")
+
+            player.src = capturaAudio
+            player.play()
+    }, 6000);
+   
+   
+    
 }else{
+   
+   document.getElementById("carregando-campeonato").style.display = "none"
        document.getElementById("um").style.display = "block"
        document.getElementById("dois").style.display = "none"
 }
 
 document.getElementById("americadosul").addEventListener("click",function(){
     
-   localStorage.setItem("continente","America Do Sul")
+   sessionStorage.setItem("continente","America Do Sul")
     window.location.href = "index.html"
+   
 })
 
 
 document.getElementById("europa").addEventListener("click",function(){
-    localStorage.setItem("continente","Europa")
+    sessionStorage.setItem("continente","Europa")
     window.location.href = "index.html"
+  
 })
 
 document.getElementById("fifacup").addEventListener("click",function(){
-    localStorage.setItem("continente","FifaCup")
+    sessionStorage.setItem("continente","FifaCup")
    window.location.href = "index.html"
  
 })
 
 document.getElementById("brasileirao").addEventListener("click",function(){
-    //localStorage.setItem("continente","Europa")
-    //window.location.href = "index.html"
-    alert("Brasileirão (Página em construção)")
+    sessionStorage.setItem("continente","Brasileirao")
+    window.location.href = "index.html"
+  
+    //alert("Brasileirão (Página em construção)")
 })
 
 
 
-
-
-
-// ACESSANDO O SERVIDOR PARA FAZER REQUISIÇÕES
+    // ACESSANDO O SERVIDOR PARA FAZER REQUISIÇÕES
 
 fetch("backend.json")
 .then(response => response.json())
@@ -47,7 +107,7 @@ fetch("backend.json")
 
 // SALVANDO A REQUISIÇÃO NO BANCO DE DADOS LOCAL
 
-var salvarLocalmente = localStorage.setItem("requisicao",JSON.stringify(data))
+var salvarLocalmente = sessionStorage.setItem("requisicao",JSON.stringify(data))
 
     console.log("A requisição foi salva no banco de dados local :)")
   
@@ -67,9 +127,9 @@ var salvarLocalmente = localStorage.setItem("requisicao",JSON.stringify(data))
 var clicouBotao = document.getElementById("btn-sortear")
 clicouBotao.addEventListener("click", function(){
 
-    var captura_times_bd_local = JSON.parse(localStorage.getItem("requisicao"))
+    var captura_times_bd_local = JSON.parse(sessionStorage.getItem("requisicao"))
 
- var capturaContinente =  localStorage.getItem("continente")
+ var capturaContinente =  sessionStorage.getItem("continente")
 
  var confronto = captura_times_bd_local.find( tbd => tbd.Continente === capturaContinente)
 
@@ -91,13 +151,22 @@ if(confronto){
 
      timesRecuperados.forEach(t => {
 
-    localStorage.setItem(capturaNome,JSON.stringify(t))
+        var capturaAudio = t.audio
+            var player = document.getElementById("audioteste")
+
+            player.src = capturaAudio
+            player.play()
+
+
+
+    sessionStorage.setItem(capturaNome,JSON.stringify(t))
 
 $("."+capturaNome).attr('src', t.imagem)
 $("#times-"+capturaNome).attr('style', "background-color: green;")
 $("#time-"+capturaNome).html(t.time)
 
-     localStorage.setItem("requisicao",JSON.stringify(captura_times_bd_local))
+
+     sessionStorage.setItem("requisicao",JSON.stringify(captura_times_bd_local))
 
 
  // alert(`O jogador ${capturaNome} escolheu o time : ${t.time}`)
@@ -127,14 +196,35 @@ $("#time-"+capturaNome).html(t.time)
 
  
 });
+    
+
+
+
+
+
+
 
 document.getElementById("reiniciar").addEventListener("click",function(){
-    localStorage.clear()
+    sessionStorage.clear()
     alert("Você apagou o Banco de Dados")
     window.location.href="index.html"
 })
 
 
+
+function mostrarCarregando(){
+
+     document.getElementById("dois").style.display = "none"
+
+
+var montarHTML = `
+
+
+<img src="img/carregando.gif" width="150px;">
+        O campeonato está <br>sendo carregado..
+`
+$("#carregando-campeonato").append(montarHTML)
+}
 
 
 
